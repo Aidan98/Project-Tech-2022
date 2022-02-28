fdf81b7f4036b22011de8c65d535305d48237c55
Aidan98 <aidanovic22@outlook.com>
2022-02-28 10:37:43 +0100

    added style_guide

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const ejs = require('ejs');
 const express = require('express'); 
-
 
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+const login = require('./routes/login')
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.static('static'))
+
+app.get('/', login) 
+
+  app.get('/register', function (req, res){
+    res.send('Registration form')
+  });
+
+  app.get('*', function(req, res){
+    res.send('error 404 not found', 404);
+  });
 
   app.listen(3000)
+
---
+++ routes/login.js
@@ -0,0 +1,7 @@
+const admin = 'Aidan';
+
+function login(req,res) {
+    res.render('login')
+}
+
+module.exports = login
