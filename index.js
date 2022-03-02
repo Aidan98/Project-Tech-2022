2f1c2dcf15c3079b83f6e30a4b2c76910cf19641
Aidan98 <aidanovic22@outlook.com>
2022-03-03 00:27:13 +0100

    reconfigured my routes

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const express = require('express');
 
 const app = express();
 
-const login = require('./routes/login')
-
+// const login = require('./routes/login')
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use(express.static('static'))
 app.use('/public', express.static(__dirname + '/public/'))
 
-app.get('/', login) 
+//ROUTES
+app.use('/', require('./routes/home'))
+app.use('/users', require('./routes/users'))
+// app.use('/login', require('./routes/login'))
+// app.use('/register', require('./routes/register'))
 
-  app.get('/register', function (req, res){
-    res.send('Registration form')
-  });
+  
 
   app.get('*', function(req, res){
-    res.send('error 404 not found', 404);
-  });
+    res.send('error 404 not found', 404)
+  })
 
   app.listen(3000)
 
