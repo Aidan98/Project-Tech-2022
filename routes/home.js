7bfbbff539b91bc1b7f2369290dbe4839a08941f
Aidan98 <aidanovic22@outlook.com>
2022-03-13 11:38:26 +0100

    commented out the protected routes as it does not work in its current state

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 // const {checkAuthenticated} = require('../config/authenticate')
 
-isAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) { return next() }
-    res.redirect("/users/login")
-  }
+// isAuthenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) { return next() }
+//     res.redirect("/users/login")
+//   }
 
-router.get('/', isAuthenticated, (req, res) => res.render('index'))
+router.get('/',  (req, res) => res.render('index'))
 
 module.exports = router
